95d1aed58d4da93c828ec7bb25e2e7e917309561
mazen moataz <mazenmoataz404@gmail.com>
2025-11-16 01:56:48 +0200

    Initial Commit

---
+++ FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Mazen Moataz on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
